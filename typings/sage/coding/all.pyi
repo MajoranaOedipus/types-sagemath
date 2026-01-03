3db449f4ab52d0ad79cef59afe8d2f85b88908fe
MajoranaOedipus <majoranaoedipus@posteo.org>
2026-01-03 19:00:11 +0800

    crypto/all.pyi cleared
    coding/all.pyi cleared

--- typings/sage/coding/all.pyi
+++ typings/sage/coding/all.pyi
@@ -1,1 +1,9 @@
-from sage.misc.lazy_import import lazy_import as lazy_import
+from sage.coding.code_constructions import (
+    permutation_action as permutation_action,
+    walsh_matrix as walsh_matrix
+)
+
+from sage.coding.linear_code import LinearCode as LinearCode
+
+from sage.coding import codes_catalog as codes
+from sage.coding import channels_catalog as channels
--- typings/sage/crypto/all.pyi
+++ typings/sage/crypto/all.pyi
@@ -1,2 +1,23 @@
-from sage.misc.lazy_import import lazy_import as lazy_import
 from sage.misc.persist import register_unpickle_override as register_unpickle_override
+
+from sage.crypto.classical import (
+    AffineCryptosystem as AffineCryptosystem,
+    HillCryptosystem as HillCryptosystem,
+    SubstitutionCryptosystem as SubstitutionCryptosystem,
+    ShiftCryptosystem as ShiftCryptosystem,
+    TranspositionCryptosystem as TranspositionCryptosystem,
+    VigenereCryptosystem as VigenereCryptosystem
+)
+
+from sage.crypto.stream import (
+    LFSRCryptosystem as LFSRCryptosystem,
+    ShrinkingGeneratorCryptosystem as ShrinkingGeneratorCryptosystem
+)
+
+from sage.crypto.lfsr import (
+    lfsr_sequence as lfsr_sequence,
+    lfsr_autocorrelation as lfsr_autocorrelation,
+    lfsr_connection_polynomial as lfsr_connection_polynomial
+)
+
+import sage.crypto.key_exchange.catalog as key_exchange
